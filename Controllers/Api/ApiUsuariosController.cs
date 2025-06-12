642f0d13feec90b976bd6dc5c544cf4e2dd07db7
Naypapasconqueso <nayeli.mezasanchez.gen23@cbtis105.edu.mx>
2025-06-12 10:06:56 -0600

    Avancee

--- Controllers/Api/ApiUsuariosController.cs
+++ Controllers/Api/ApiUsuariosController.cs
@@ -21,10 +21,18 @@ public class ApiUsuariosController: ControllerBase
     }
 
     [HttpGet]
-    public IActionResult ListarUsuarios()
+    public IActionResult ListarUsuarios(string? texto)
     {
         var filter = FilterDefinition<Usuario>.Empty;
+        if (!string.IsNullOrWhiteSpace(texto))
+        {
+            var filterNombre = Builders<Usuario>.Filter.Regex(u => u.Nombre, new BsonRegularExpression(texto, "i"));
+            var filterCorreo = Builders<Usuario>.Filter.Regex(u => u.Correo, new BsonRegularExpression(texto, "i"));
+            filter = Builders<Usuario>.Filter.Or(filterNombre,filterCorreo);
+        }
+
         var list = this.collection.Find(filter).ToList();
+        
         return Ok(list);
     }
 }
